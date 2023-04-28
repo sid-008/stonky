8f4c06986474f4c54dae2d10761469f0472d015e
Arjun Maniyani <arjun.maniyani@Arjuns-MacBook-Air.local>
2023-04-28 22:33:22 +0530

    added backend var ticker

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,9 @@ def ping():
     return "<p>pong</p>"
 
 
-@app.route("/get", methods = ['GET'])
-def get():
-    msft = yf.Ticker("MSFT")
+@app.route('/<stock>', methods = ['GET'])
+def get(stock):
+    msft = yf.Ticker(stock)
     info = msft.info
     info_json = json.dumps(info, indent=4)
     return jsonify(info_json)
